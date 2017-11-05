55bdd4b353efbb5a202adfecbc01b7d7b918a0c3
Chris Hill-Scott <me@quis.cc>
2017-11-05 10:24:14 +0000

    WIP2

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,3 +7,32 @@ $(() => GOVUK.modules.start());
 $(() => $('.error-message').eq(0).parent('label').next('input').trigger('focus'));
 
 $(() => $('.banner-dangerous').eq(0).trigger('focus'));
+
+console.log('hey');
+
+$(function() {
+  var c = $(".fullscreen-container");
+  var ch = c.innerHeight();
+  var cTop = c.offset().top;
+  var shim = $("<div/>");
+  shim.css({
+    'height': ch,
+    'width': 1,
+    'position': 'absolute',
+    'top': cTop
+  });
+  c.after(shim);
+
+  $(window).on('scroll resize', function() {
+    var vp = $(window).height();
+    var st = $('html, body').scrollTop();
+    c.css(
+      'max-height',
+      Math.min(
+        vp - cTop + st,
+        ch
+      )
+    );
+  });
+
+});
